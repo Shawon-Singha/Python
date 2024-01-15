396dc209ae015282ea700614b7307f803ce3bb62
Shawon Singha <shawonsingha10@gmail.com>
2024-01-15 14:53:57 +0600

    update

--- Tuples/1tuples.py
+++ Tuples/1tuples.py
@@ -1,3 +1,9 @@
+'''
+tuples and list are almost same .....
+but tuples is more faster than list
+We can not change value of tuples like list
+'''
+
 students = ((10,20,30),(11,12,13),(14,15,16))
 
 print(students)  #print all list
