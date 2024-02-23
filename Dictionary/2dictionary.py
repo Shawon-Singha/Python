75fefb516e5358506a0d041ed6210aae8feaf192
Shawon-Singha <shawonsingha10@gmail.com>
2024-02-23 10:41:07 +0600

    update

---
+++ Dictionary/2dictionary.py
@@ -0,0 +1,9 @@
+teacherId = {
+    "101" : "Shawon",
+    "102" : 2635365,
+    "103" : "Magura"
+}
+
+print(teacherId) #print total dictionary
+print(teacherId.get("103")) #for access indivisual value
+print(teacherId.get("104","Not a valid key")) #if given not a valid key
