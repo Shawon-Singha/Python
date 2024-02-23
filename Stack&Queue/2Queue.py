d651322be93f94174162d8a170fac69d615b9096
Shawon-Singha <shawonsingha10@gmail.com>
2024-02-23 12:23:47 +0600

    update

---
+++ Stack&Queue/2Queue.py
@@ -0,0 +1,18 @@
+#using deque...
+
+from collections import deque
+
+bank = deque(["Shawon","karim","Singha"])
+print(bank)
+
+bank.pop()
+print(bank)
+
+
+
+# #using list
+# bank = []
+# bank.append("Shawon")
+# bank.append("Anik")
+# bank.append("Singha")
+# print(bank)
