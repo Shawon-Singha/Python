e92c4fa10881e6003cbea7b8ea92803d182731d2
Shawon-Singha <shawonsingha10@gmail.com>
2024-02-23 10:57:42 +0600

    update

---
+++ Tuples/2tuples.py
@@ -0,0 +1,9 @@
+teacher = (
+    "Shawon", "Anik",202,"Singha"
+)
+
+print(teacher) #print that tuples list
+print(teacher[0])  #access indivisual index
+
+#print from any index to last
+print(teacher[1:])  #print from 1 index to last
