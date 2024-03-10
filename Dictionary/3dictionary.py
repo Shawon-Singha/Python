d4df51b57ada386ebd81004bcbfcd33289b6a79f
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-10 14:08:23 +0600

    update

--- Dictionary/3dictionary.py
+++ Dictionary/3dictionary.py
@@ -1,22 +1,32 @@
-list = []
+list1 = [10,20,30,40] 
 
-while  True:
-    value = int(input("Enter the value : "))
-    if value == 0:
-        movie = input("Enter your movie name : ")
-        list.append(movie)
-    elif value == 1:
-        break
-    else:
-        print("Invalid input")
+# #for user input, comment out thosse line
+
+# list = []
+# while  True:
+#     value = int(input("Enter the value : "))
+#     if value == 0:
+#         movie = input("Enter your movie name : ")
+#         list.append(movie)
+#     elif value == 1:
+#         break
+#     else:
+#         print("Invalid input")
+
+update_list = ['The Godfather','Pulp Fiction','Inception','The Dark Knight'
+'Titanic','Jurassic Park','The Matrix,Avatar']
 
 info = {
     'Key' : "Value",
     '101' : "Shawon",
     '102' : "Anik",
-    '103' : list   ##passing list inside a dictionary
+    '103' : list1  ##passing list inside a dictionary
+    
 }
 
 print(type(info))
 
 print(info["103"])
+
+info['103'] = update_list
+print(info['103'])
