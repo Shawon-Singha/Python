301874b98101a2a38b989a958275d2192ac192db
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-10 10:48:54 +0600

    update

--- Series/3practice.py
+++ Series/3practice.py
@@ -1,8 +1,12 @@
 '''
 count the number of student get 'A' in the following tuple
 ['C','D','A','A','B','B','A']
+
+and store those in ssorted order
 '''
 
 tup = ['C','D','A','A','B','B','A']
-
+tup.sort()
 print(tup.count('A'))
+
+print(tup)
