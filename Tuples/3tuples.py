605c714c0627378803c0764127762d9d43142c1b
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-10 10:22:07 +0600

    update

---
+++ Tuples/3tuples.py
@@ -0,0 +1,10 @@
+tup = (1,2,3,4,5,3,6,7,3)
+
+#print from starting to n-1 
+print(tup[0:3]) 
+
+#showing the value is in which index
+print(tup.index(1))
+
+#how many time have the target value
+print((tup.count(3)))
