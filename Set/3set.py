ece48b8a649f5fda3e69534b5e49543c47d7f692
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-11 19:27:49 +0600

    update

---
+++ Set/3set.py
@@ -0,0 +1,16 @@
+num1 = {1,2,2,3,4,4,"Shawon","Anik",5}
+print(num1)
+
+num2 = set()
+num2.add(10)
+num2.add(20)
+num2.add(30)
+print(num2)
+
+#first in first out
+num2.pop()
+print(num2)
+
+#clear the set
+num2.clear()
+print(num2)
