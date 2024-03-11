3030d98d54002908f7de5b2eb901b3008d3a942e
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-11 13:54:33 +0600

    update

---
+++ Dictionary/5dictionr.py
@@ -0,0 +1,15 @@
+list = [3.71,3.63,3.71,3.91,3.76]
+
+teacher = {
+    'key' : 'Value',
+    'result' : list,
+    'update' : {
+        '6' : 4.00,
+        '7' : 4.00,
+        '8' : 4.00,
+        '9' : 4.00
+    },
+    'target' : "Teacher"
+}
+
+print(teacher)
