1818650aaf51cdcf101d6319ffaa3a7e568177a1
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-11 19:43:05 +0600

    update

---
+++ Pratice/4practice.py
@@ -0,0 +1,6 @@
+#here list of subject.Assume one classroom is required for 1 subject.How many classroom needed
+
+sub = {'python','java','C++','Python','javascript','java','python',
+    'java','C++','C'}
+
+print(f"Classroom needed : {len(sub)}")
