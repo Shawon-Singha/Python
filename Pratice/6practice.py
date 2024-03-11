f100ee9ca4da45d4f78bf35e224ca608df26adff
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-11 19:54:09 +0600

    update

---
+++ Pratice/6practice.py
@@ -0,0 +1,16 @@
+marks = {}
+
+phy = int(input("Enter your physics mark: "))
+marks.update({'Physics' : phy})
+
+ches = int(input("Enter your chemistry mark: "))
+marks.update({'Chemistry' : ches})
+
+math = int(input("Enter your math mark: "))
+marks.update({'Math' : math})
+
+ict = int(input("Enter your ict mark: "))
+marks.update({'ICT' : ict})
+
+print(marks)
+
