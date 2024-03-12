90c5d574d1cc306a9520fbd2a76c602b0cdf1845
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-12 19:54:40 +0600

    update

--- Set/6set.py
+++ Set/6set.py
@@ -1,14 +1,19 @@
 semester1 = set()
 semester2 = set()
 
-List = ['Physics','Chemistry','Math','Biology']
+sub_list = []
 
 print("For terminate loop enter the value of -1")
+while True:
+    sub = input("Enter your subject name : ")
+    if sub == '-1': break
+    sub_list.append(sub)
+
 i = 0
 while True:
-    if i == len(List): break
-    x = int(input(f"Enter your first semester mark for {List[i]} : "))
-    y = int(input(f"Enter your second semester mark for {List[i]} : "))
+    if i == len(sub_list): break
+    x = int(input(f"Enter your first semester mark for {sub_list[i]} : "))
+    y = int(input(f"Enter your second semester mark for {sub_list[i]} : "))
 
     semester1.add(x)
     semester2.add(y)
@@ -17,7 +22,7 @@ while True:
 if semester1 == semester2:
     print("Going with your this form")
 
-elif semester1 > semester2:
+elif semester1 < semester2:
     print("Your first semester was good")
 else:
     print("Your second semester was good")
