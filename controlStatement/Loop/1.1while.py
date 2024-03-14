5444a06e98e5723dab9986cd9eeb7b5c5cd2ec2c
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-14 21:02:08 +0600

    update

--- controlStatement/Loop/1.1while.py
+++ controlStatement/Loop/1.1while.py
@@ -1,7 +1,7 @@
 li = [1,4,9,16,25,36,49,64,81,100]
 
+#traverses
 i = 0
-
 while i < len(li):
     print(li[i],end= " ")
     i += 1
---
+++ controlStatement/Loop/1.2while.py
@@ -0,0 +1,9 @@
+li = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100}
+
+value = int(input("Enter your desired value: "))
+
+if value in li:
+    print("Found")
+    
+else:
+    print("Not Found")
