2961ebb107016ceec6618232d677425736f369d2
Shawon-Singha <shawonsingha10@gmail.com>
2024-03-15 16:30:50 +0600

    update

--- FileInputOutput/1pyDemo.py
+++ FileInputOutput/1pyDemo.py
@@ -3,6 +3,14 @@ f = open("1demo.txt","r")
 data = f.read() #read entire file
 print(data)
 
-value = f.readline() #read line bby line
-print(value)
+
+"""
+after read data one time,It will be in last .....
+if read second time ,it will print new line just
+"""
+value1 = f.readline() #read first line
+print(value1)
+
+value2 = f.readlines() #read all line by line
+print(value2)
 f.close()
